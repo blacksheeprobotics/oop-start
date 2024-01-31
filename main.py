0770b571745d3d5a8682f0e1bee7460b156fd9c5
RJW <blacksheeprobotics@gmail.com>
2024-01-31 00:02:23 -0500

    video exercise on python methods, attributes and importing libraries

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# intial main.py set for version control
+import another_module
+print(another_module.another_module)
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(25)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
+
